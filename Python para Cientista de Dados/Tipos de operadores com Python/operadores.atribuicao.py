6c0a184eed576db4a4b0b4eddec05ecc359f4ffa
Fabiola Spredemann <fabispredemann@gmail.com>
2022-10-14 13:45:41 -0300

    Operadores de atribuição

---
+++ Python para Cientista de Dados/Tipos de operadores com Python/operadores.atribuicao.py
@@ -0,0 +1,28 @@
+saldo = 500
+
+print(saldo)
+
+saldo = 200
+
+print(saldo)
+
+saldo += 10
+print(saldo)
+
+saldo -= 5
+print(saldo)
+
+saldo *= 10
+print(saldo)
+
+saldo /= 2
+print(saldo)
+
+saldo //= 2
+print(saldo)
+
+saldo %= 3
+print(saldo)
+
+saldo **= 2
+print(saldo)
