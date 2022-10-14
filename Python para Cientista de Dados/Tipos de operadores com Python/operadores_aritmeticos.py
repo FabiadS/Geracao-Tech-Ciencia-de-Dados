54780adba53e7ec0cb958932c2b7ecb51b72edf4
Fabiola Spredemann <fabispredemann@gmail.com>
2022-10-14 13:25:08 -0300

    Operadores aritméticos

---
+++ Python para Cientista de Dados/Tipos de operadores com Python/operadores_aritmeticos.py
@@ -0,0 +1,13 @@
+produto_1 = 20
+produto_2 = 10
+
+print(f"Soma: {produto_1 + produto_2}")
+print(f"Subtracao: {produto_1 - produto_2}")
+print(f"Multiplicacao: {produto_1 * produto_2}")
+print(f"Divisao com resultado inteiro: {produto_1 // produto_2}")
+print(f"Divisao: {produto_1 / produto_2}")
+print(f"Resto da divisao: {produto_1 % produto_2}")
+print(f"Exponenciacao: {produto_1 ** produto_2}")
+
+
+
