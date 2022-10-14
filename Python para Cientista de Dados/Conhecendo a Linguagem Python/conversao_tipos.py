f7e77e0ffc345b490aaa1fe65c679d74cb4a132e
Fabiola Spredemann <fabispredemann@gmail.com>
2022-10-14 09:46:27 -0300

    Conversão de tipos

---
+++ Python para Cientista de Dados/Conhecendo a Linguagem Python/conversao_tipos.py
@@ -0,0 +1,12 @@
+print(int(1.9565)) #convertendo para int
+print(int("10")) 
+print(float("10.60")) 
+print(float(100)) #convertendo para float
+
+valor = 10
+valor_str = str(valor) #convertendo para string
+print(type(valor)) #retorna o tipo da variavel valor
+print(type(valor_str)) #retorna o tipo da variável valor_str
+
+print(10 / 2) #retorna flutuante
+print(10 //2) #retorna a parte inteira
