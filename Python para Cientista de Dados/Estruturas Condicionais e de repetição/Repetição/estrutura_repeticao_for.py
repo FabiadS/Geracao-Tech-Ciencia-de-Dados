caa4346c5221b8a91d247e6f9791cc6303ac6108
Fabiola Spredemann <fabispredemann@gmail.com>
2022-10-17 11:58:00 -0300

    Range e iterável

--- Python para Cientista de Dados/Estruturas Condicionais e de repetição/Repetição/estrutura_repeticao_for.py
+++ Python para Cientista de Dados/Estruturas Condicionais e de repetição/Repetição/estrutura_repeticao_for.py
@@ -1,9 +1,14 @@
 texto = input("Informe um texto: ")
 VOGAIS = "AEIOU"
 
+#exemplo utilizando um iterável
 for letra in texto:
     if letra.upper() in VOGAIS:
         print(letra, end="")
 
 else:
     print()
+
+#exemplo utilizando a função bult-in range
+for numero in range(0, 51, 5):
+    print(numero, end=" ")
