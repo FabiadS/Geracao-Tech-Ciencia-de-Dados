bb7ebf076a60833925e62837b43e67bc41e33e86
Fabiola Spredemann <fabispredemann@gmail.com>
2022-10-17 10:46:53 -0300

    Operadores de Associação

---
+++ Python para Cientista de Dados/Tipos de operadores com Python/operadores_associacao.py
@@ -0,0 +1,6 @@
+frutas = ["limao" "uva"]
+curso = "curso de python"
+
+print(f'A fruta "laranja" está presente em frutas: {"laranja" in frutas}')
+print(f'A fruta "morango" não está presente em frutas: {"morango" not in frutas}')
+print("Python" in curso)
