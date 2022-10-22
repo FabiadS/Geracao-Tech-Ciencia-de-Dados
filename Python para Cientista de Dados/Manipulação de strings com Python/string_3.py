978c936d28818bb325285c6312074d09edd812e1
Fabiola Spredemann <fabispredemann@gmail.com>
2022-10-22 10:51:57 -0300

    Fatiamento de string

---
+++ Python para Cientista de Dados/Manipulação de strings com Python/string_3.py
@@ -0,0 +1,10 @@
+nome = "Fabiola Augusta Dahlke Spredemann"
+
+print(nome[0]) # pega o primeiro caractere
+print(nome[:7]) # pega até o 7 - 1
+print(nome[8:]) # pega do oitavo caractere até o ultimo
+print(nome[10:15]) # do 10 ao 14
+print(nome[10:16:2]) # do 10 ao 15 de 2 em 2
+print(nome[:]) # toda a string
+print(nome[::-1]) #espelhamento
+print(nome[-1]) # pega o ultimo caractere
