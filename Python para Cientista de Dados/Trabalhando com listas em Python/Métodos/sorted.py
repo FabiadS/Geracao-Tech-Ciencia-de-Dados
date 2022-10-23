18a65ad0293d14ac1eb2aa5d5864bf982cf9f2e0
Fabiola Spredemann <fabispredemann@gmail.com>
2022-10-23 11:17:10 -0300

    Método sorted

---
+++ Python para Cientista de Dados/Trabalhando com listas em Python/Métodos/sorted.py
@@ -0,0 +1,5 @@
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+#ordenar iteráveis
+print(sorted(linguagens, key=lambda x: len(x)))  # ["c", "js", "java", "python", "csharp"]
+print(sorted(linguagens, key=lambda x: len(x), reverse=True))  # ["python", "csharp", "java", "js", "c"]
