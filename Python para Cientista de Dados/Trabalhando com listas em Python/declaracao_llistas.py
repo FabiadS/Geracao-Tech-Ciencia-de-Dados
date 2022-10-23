9f3b9b6f1ed391a4391b2ab99d22eec80022a2ab
Fabiola Spredemann <fabispredemann@gmail.com>
2022-10-23 10:16:12 -0300

    Declaração de listas

---
+++ Python para Cientista de Dados/Trabalhando com listas em Python/declaracao_llistas.py
@@ -0,0 +1,14 @@
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+
+frutas = [] #declarando lista vazia
+print(frutas)
+
+letras = list("python") # transofrmando a palavra python em uma lista com os carateres
+print(letras)
+
+numeros = list(range(10)) # lista e numeros inteiros (0 a 9)
+print(numeros)
+
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True] # lista dos atributos de um carro
+print(carro)
