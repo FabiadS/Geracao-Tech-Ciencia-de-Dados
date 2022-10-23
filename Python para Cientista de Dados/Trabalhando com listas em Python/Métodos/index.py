ae71c374adf0e299cde8b7200a47350f4c136153
Fabiola Spredemann <fabispredemann@gmail.com>
2022-10-23 11:03:38 -0300

    Método index

---
+++ Python para Cientista de Dados/Trabalhando com listas em Python/Métodos/index.py
@@ -0,0 +1,4 @@
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+print(linguagens.index("java"))  # encontrar a primeira ocorrência da string 'java' # 3
+print(linguagens.index("python"))  # 0
