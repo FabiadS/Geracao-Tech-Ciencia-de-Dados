27270cc431980cd545b834b132ee264531c2f70d
Fabiola Spredemann <fabispredemann@gmail.com>
2022-10-23 11:09:30 -0300

    Método remove

--- Python para Cientista de Dados/Trabalhando com listas em Python/Métodos/remove.py
+++ Python para Cientista de Dados/Trabalhando com listas em Python/Métodos/remove.py
@@ -1,5 +1,5 @@
 linguagens = ["python", "js", "c", "java", "csharp"]
 
-linguagens.remove("c")
+linguagens.remove("c") #remove a primeira ocorrência de 'c'
 
 print(linguagens)  # ["python", "js", "java", "csharp"]
