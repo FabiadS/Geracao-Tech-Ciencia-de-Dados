4ab19dad5d18d4f7c8dae81df75232390eeb0431
Fabiola Spredemann <fabispredemann@gmail.com>
2022-10-24 08:27:08 -0300

    Declarando conjuntos set

---
+++ Python para Cientista de Dados/Explorando conjuntos com Python/declarando_conjuntos.py
@@ -0,0 +1,13 @@
+#elimina os repetidos pois é uma coleção de valores únicos
+
+numeros = set([1, 2, 3, 1, 3, 4])
+print(numeros)  
+
+letras = set("abacaxi")
+print(letras)  
+
+carros = set(("palio", "gol", "celta", "palio"))
+print(carros) 
+
+linguagens = {"python", "java", "python"}
+print(linguagens)
