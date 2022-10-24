74e200b5c1442cd516ba57086c1df2b384141ef3
Fabiola Spredemann <fabispredemann@gmail.com>
2022-10-24 08:44:25 -0300

    Diferença simétrica

---
+++ Python para Cientista de Dados/Explorando conjuntos com Python/Métodos/symetric_difference.py
@@ -0,0 +1,7 @@
+from cgi import print_directory
+
+
+conjunto_a = {1, 2, 3}
+conjunto_b = {2, 3, 4}
+
+print(conjunto_a.symmetric_difference(conjunto_b)) # todos os elementos que não estão na intersecção
