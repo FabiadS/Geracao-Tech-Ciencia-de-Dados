2ce6e038550dc45884a3cf3b8f6daffe2b3d4102
Fabiola Spredemann <fabispredemann@gmail.com>
2022-11-03 09:39:26 -0300

    Declarando dicionários

---
+++ Python para Cientista de Dados/Aprendendo a utilizar dicionários em Python/declarando.py
@@ -0,0 +1,8 @@
+pessoa = {"nome": "Guilherme", "idade": 28}
+print(pessoa)
+
+pessoa = dict(nome="Guilherme", idade=28) #usando o construtor dict
+print(pessoa)
+
+pessoa["telefone"] = "3333-1234"  # adicionando uma nova chave no dicionário pessoa
+print(pessoa)
