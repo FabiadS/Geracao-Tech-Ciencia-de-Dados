d499e43e20075ed7fe156c103b28de5ceb368225
Fabiola Spredemann <fabispredemann@gmail.com>
2022-11-03 10:13:24 -0300

    Método pop

---
+++ Python para Cientista de Dados/Aprendendo a utilizar dicionários em Python/Métodos/pop.py
@@ -0,0 +1,8 @@
+#remove uma chave do dicionário
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+resultado = contatos.pop("guilherme@gmail.com")  # {'nome': 'Guilherme', 'telefone': '3333-2221'}
+print(resultado)
+
+resultado = contatos.pop("guilherme@gmail.com", {})  # caso n exista, retorna o valor padrão {}
+print(resultado)
