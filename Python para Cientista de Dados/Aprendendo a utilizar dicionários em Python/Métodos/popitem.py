7011f38dd519567a249e121cdffbfef31903ca0c
Fabiola Spredemann <fabispredemann@gmail.com>
2022-11-03 10:13:35 -0300

    Método popitem

---
+++ Python para Cientista de Dados/Aprendendo a utilizar dicionários em Python/Métodos/popitem.py
@@ -0,0 +1,7 @@
+#não precisa informar a chave, ele remove na sequencia
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+resultado = contatos.popitem()  # ('guilherme@gmail.com', {'nome': 'Guilherme', 'telefone': '3333-2221'})
+print(resultado)
+
+# contatos.popitem()  # KeyError caso esteja vazio
