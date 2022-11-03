b91b917ec3e8ce731e43f43daccf4eebc6371bb8
Fabiola Spredemann <fabispredemann@gmail.com>
2022-11-03 10:03:42 -0300

    Método items

--- Python para Cientista de Dados/Aprendendo a utilizar dicionários em Python/Métodos/items.py
+++ Python para Cientista de Dados/Aprendendo a utilizar dicionários em Python/Métodos/items.py
@@ -1,3 +1,4 @@
+#retorna como uma lista
 contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
 
 resultado = contatos.items()  # dict_items([('guilherme@gmail.com', {'nome': 'Guilherme', 'telefone': '3333-2221'})])
