021956aac734520a08db9714db79d59538590d30
Fabiola Spredemann <fabispredemann@gmail.com>
2022-11-03 10:03:34 -0300

    Método keys

---
+++ Python para Cientista de Dados/Aprendendo a utilizar dicionários em Python/Métodos/keys.py
@@ -0,0 +1,5 @@
+#retorna apenas as chaves
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+resultado = contatos.keys()  # dict_keys(['guilherme@gmail.com'])
+print(resultado)
