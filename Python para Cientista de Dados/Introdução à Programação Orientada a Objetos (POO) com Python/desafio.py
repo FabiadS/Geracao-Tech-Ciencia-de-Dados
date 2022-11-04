122c2ea04e31d10424703112341c5a75d2c8aa55
Fabiola Spredemann <fabispredemann@gmail.com>
2022-11-04 14:11:51 -0300

    Desafio bicicletaria

---
+++ Python para Cientista de Dados/Introdução à Programação Orientada a Objetos (POO) com Python/desafio.py
@@ -0,0 +1,5 @@
+# João tem uma bicicletaria e gostaria de registrar as vendas de
+# suas bicicletas. Crie um programa onde João informe: cor,
+# modelo, ano e valor da bicicleta vendida. Uma bicicleta pode:
+# buzinar, parar e correr. Adicione esses comportamentos!
+
